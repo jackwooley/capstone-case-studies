43338847324ebb56edec51fa3fc1152711262afb
low-ethan <ethanthelow@gmail.com>
2023-04-05 18:49:16 -0600

    Simple Bucket

--- sentence-classifier/eth_class.py
+++ sentence-classifier/eth_class.py
@@ -19,20 +19,7 @@ def append_to_df(df, section, val):
     df =df.append(pd.read_csv('up_copy_txt/'+section+val+'.csv'))
     return df
 
-df = pd.read_csv("fin_data.csv")  # i limited it to 500 for speed of trainig on a cpu, read all when fully training
-
-print(len(df))
-df = append_to_df(df, 'satisfaction', '5')
-df = append_to_df(df, 'satisfaction', '1')
-
-print(len(df))
-
-label_type = 'satisfaction'
-
-train_dev, test = train_test_split(df, test_size=0.2)
-train, dev = train_test_split(train_dev, test_size=0.2)
-
-def load_df_to_sentences(df: pd.DataFrame):
+def load_df_to_sentences(df: pd.DataFrame, label_type):
     sentences = []
     for index, row in df.iterrows():
         sentence = flair.data.Sentence(row['text'])
@@ -41,33 +28,57 @@ def load_df_to_sentences(df: pd.DataFrame):
         sentences.append(sentence)
     return sentences
 
-train_sentences = load_df_to_sentences(train)
-test_sentences = load_df_to_sentences(test)
-dev_sentences = load_df_to_sentences(dev)
+def main():
+    df = pd.read_csv("fin_data.csv")  # i limited it to 500 for speed of training on a cpu, read all when fully training
+
+    # Adding Upsampled data
+    print(len(df))
+    df = append_to_df(df, 'satisfaction', '5')
+    df = append_to_df(df, 'satisfaction', '1')
+    print(len(df))
+
+    # Changing to 3 bucket approach
+    df.loc[df["satisfaction_bucket"] == 2, "satisfaction_bucket"] = 1
+    df.loc[df["satisfaction_bucket"] == 4, "satisfaction_bucket"] = 5
+
+    print(df["satisfaction_bucket"].value_counts())
+
+    label_type = 'satisfaction'
+
+    train_dev, test = train_test_split(df, test_size=0.2)
+    train, dev = train_test_split(train_dev, test_size=0.2)
 
 
 
-# load corpus containing training, test and dev data and if CSV has a header, you can skip it
-corpus: Corpus = Corpus(train_sentences, dev_sentences, test_sentences)
+    train_sentences = load_df_to_sentences(train, label_type)
+    test_sentences = load_df_to_sentences(test, label_type)
+    dev_sentences = load_df_to_sentences(dev, label_type)
 
 
 
-# 3. create the label dictionary
-label_dict = corpus.make_label_dictionary(label_type=label_type)
+    # load corpus containing training, test and dev data and if CSV has a header, you can skip it
+    corpus: Corpus = Corpus(train_sentences, dev_sentences, test_sentences)
 
-# 4. initialize transformer document embeddings (many models are available)
-document_embeddings = TransformerDocumentEmbeddings('distilbert-base-uncased', fine_tune=True)  # transformer embeddings are hard core, awesome, you can experiment
 
-# 5. create the text classifier
-classifier = TextClassifier(document_embeddings, label_dictionary=label_dict, label_type=label_type)
 
-# 6. initialize trainer
-trainer = ModelTrainer(classifier, corpus)
+    # 3. create the label dictionary
+    label_dict = corpus.make_label_dictionary(label_type=label_type)
 
-# 7. run training with fine-tuning
-trainer.fine_tune('./test_distilbert',
-                  learning_rate=5.0e-5,  # another good one to mess with
-                  mini_batch_size=4,  # increase this, higher for a cpu, don't go above 8 on a gpu, sometimes get problem
-                  max_epochs=10,  # mess with this
-                  )
+    # 4. initialize transformer document embeddings (many models are available)
+    document_embeddings = TransformerDocumentEmbeddings('distilbert-base-uncased', fine_tune=True)  # transformer embeddings are hard core, awesome, you can experiment
 
+    # 5. create the text classifier
+    classifier = TextClassifier(document_embeddings, label_dictionary=label_dict, label_type=label_type)
+
+    # 6. initialize trainer
+    trainer = ModelTrainer(classifier, corpus)
+
+    # 7. run training with fine-tuning
+    trainer.fine_tune('./test_distilbert_3_bucket',
+                      learning_rate=5.0e-5,  # another good one to mess with
+                      mini_batch_size=4,  # increase this, higher for a cpu, don't go above 8 on a gpu, sometimes get problem
+                      max_epochs=10,  # mess with this
+                      )
+
+if __name__ == '__main__':
+    main()
